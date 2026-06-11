6f4fec87b302453a274caad4f6c646189fe320a6
hiro <hiroto117@Hirotovivobook.localdomain>
2026-06-12 00:02:54 +0900

    update

--- chat.py
+++ chat.py
@@ -12,21 +12,23 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def chatbot():
     print("今の気分はどうですか？？")
-    while True:
 
-        conversation_history = []
+    conversation_history = []
+    while True:
         user_input = input("User: ")
         # 会話履歴をリストに保存
         if user_input.lower() == "exit":
             print("今日もお疲れ様でした.")
             break
-        conversation_history.append(user_input)
+        conversation_history.append({"role": "user", "content": user_input})
 
         response = client.responses.create(
             model="gpt-5-mini", instructions=instructions, input=user_input
         )
         print(f"GPT:  {response.output_text}")
-        conversation_history.append(response.output_text)
+        conversation_history.append(
+            {"role": "assistant", "content": response.output_text}
+        )
 
 
 if __name__ == "__main__":
