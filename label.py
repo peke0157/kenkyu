77bf465c0e4584052dff92b989cdabdc5252de02
hiro <hiroto117@Hirotovivobook.localdomain>
2026-06-18 18:56:26 +0900

    update labelprompt

--- label.py
+++ label.py
@@ -2,9 +2,20 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import json
 
 load_dotenv()
-prompt_path = Path("")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+prompt_path = Path("prompts/labelprompt.txt")
 instructions = prompt_path.read_text(encoding="utf-8")
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+corpus_path = Path("japanese-daily-dialogue/data/topic1.json")
+data = json.loads(corpus_path.read_text(encoding="utf-8"))
+
+
+response = client.responses.create(
+    model="gpt-5-mini",
+    instructions=instructions
+    input=json.dumps()
+)
