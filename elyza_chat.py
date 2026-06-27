e8fdb1f4b015fb6f5a268bacbd40682d205f7630
hiro <hiroto117@Hirotovivobook.localdomain>
2026-06-27 19:30:49 +0900

    Llamaをインストール

---
+++ elyza_chat.py
@@ -0,0 +1,14 @@
+import torch
+from pathlib import Path
+import json 
+from transformers import AutoModel
+
+prompt_path = Path("prompts/keicyo.txt")
+
+
+model=("elyza/Llama-3-ELYZA-JP-8B-GGUF",dtype="auto")
+
+llm.create_chat_completion(
+    message = 
+)
+
