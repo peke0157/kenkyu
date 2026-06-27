d6ff2270aaaecf43f0aea6a260302337e2815727
hiro <hiroto117@Hirotovivobook.localdomain>
2026-06-27 23:04:10 +0900

    elaza.pyの会話作成（途中）

--- elyza_chat.py
+++ elyza_chat.py
@@ -1,14 +1,45 @@
 import torch
 from pathlib import Path
 import json 
-from transformers import AutoModel
+from llama_cpp import Llama
 
 prompt_path = Path("prompts/keicyo.txt")
+instructions = prompt_path.read_text(encoding="utf-8")
 
-
-model=("elyza/Llama-3-ELYZA-JP-8B-GGUF",dtype="auto")
-
-llm.create_chat_completion(
-    message = 
+llm = Llama.from_pretrained(
+    model_path="elyza/Llama-3-ELYZA-JP-8B-GGUF",
+    chat_format="Llama-3",
+    n_ctx=1024,
 )
+
+label_path = Path("outputs/label_output.json")
+label1_path = Path("outputs/label_output_101-200.json")
+
+def load_label_data(file_path, file1_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        file_data = json.load(f)
+    with open(file1_path, "r", encoding="utf-8") as f1:
+        file1_data = json.load(f1)
+    return file_data, file1_data
+
+def chatbot():
+    print("今日はどんな1日でしたか？")
+    
+    conversation_history = []
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() == "fin":
+            print("今日もお疲れ様でした。")
+            break
+        
+        conversation_history.append({"role": "user", "content": user_input})
+        load_label_data()
+        
+        llm.create_chat_completion(
+            message = [
+                {"role": "system", "content": instructions},
+                {"role": "user", "content": user_input}
+            ]
+            
+        )
 
