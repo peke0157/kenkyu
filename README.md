39ec8cdf7511d6aeb21c8b6544fc12b431badc01
hiro <hiroto117@Hirotovivobook.localdomain>
2026-06-28 12:20:53 +0900

    READMEの作成

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kenkyu
 研究・学習用リポジトリ
+
+# 開発環境
+- Python 3.13.5
+- Numpy 2.4.6
+- Ubuntu-24.04.1 LTS
+ 
+
+# データセットについて
+本プロジェクトのモデル学習および評価には「[日本語日常対話コーパス（Japanese-Daily-Dialogue）](https://github.com/jqk09a/japanese-daily-dialogue)」を利用しています。
+
+データセットはライセンスの都合上、本リポジトリには含まれていません。公式のリポジトリからダウンロードしてください。
--- label/json_csv.py
+++ label/json_csv.py
@@ -1,7 +1,33 @@
 import pandas as pd
+from pathlib import Path
+import json
 
+corpus_path = Path("../japanese-daily-dialogue/data/topic1.json")
+with corpus_path.open("r", encoding="utf-8") as f:
+    data = json.load(f)
+    
+# utteranceをリストにする関数
+def list_dataset(utterances):
+    
+    label = []
+    for dialogue_data in utterances:
+        utterance_data = dialogue_data["utterance"]
+        label.extend(utterance_data)
+    return label
+    
+# リストをcsvにする関数
+def save_csv(label):
+    label.to_csv("../outputs/label.csv", encoding="utf-8")
+
+def main():
+    all_list = []
+    for dialogue in data[:200]:
+        label_list = list_dataset(dialogue["utterances"])
+        all_list.extend(label_list)
+"""
 df = pd.read_json("../outputs/label_output.json", encoding="utf-8")
 df_1 = pd.read_json("../outputs/label_output_101-200.json", encoding="utf-8")
 
 df.to_csv("../outputs/label_output.csv", encoding="utf-8")
 df_1.to_csv("../outputs/label_output_101-200.csv", encoding="utf-8")
+"""
