ad040709b18e930faec502480ef2207079bc9142
hiro <hiroto117@Hirotovivobook.localdomain>
2026-07-02 20:26:56 +0900

    bert学習データ反映

--- chat_keicyo.py
+++ chat_keicyo.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 import json
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 load_dotenv()
 prompt_path = Path("prompts/keicyo.txt")
@@ -10,16 +11,13 @@ instructions = prompt_path.read_text(encoding="utf-8")
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-label_path = Path("outputs/label_output.json")
-label_path1 = Path("outputs/label_output_101-200.json")
 
 def load_label_data(file_path, file_path1):
-    with open(file_path, "r", encoding="utf-8") as f:
-        file_data = json.load(f)
-    with open(file_path1, "r", encoding="utf-8") as f1:
-        file_data1 = json.load(f1)
+    bert_path = "./my_custom_bert"
+    bert_tokenizer = AutoTokenizer.from_pretrained(bert_path)
+    bert_model = AutoModelForSequenceClassification.from_pretrained(bert_path)
         
-    return file_data, file_data1
+    
     
 def chatbot():
     print("今の気分はどうですか？？")
