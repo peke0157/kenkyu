e3e75b146fc89b4b821e647858d0bce46d9b4121
hiro <hiroto117@Hirotovivobook.localdomain>
2026-07-05 16:32:51 +0900

    自己開示モデルの反映・会話生成

--- chat_keicyo.py
+++ chat_keicyo.py
@@ -4,18 +4,18 @@ import os
 from pathlib import Path
 import json
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
 load_dotenv()
 prompt_path = Path("prompts/keicyo.txt")
-instructions = prompt_path.read_text(encoding="utf-8")
+base_prompt = prompt_path.read_text(encoding="utf-8")
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-def load_label_data(file_path, file_path1):
-    bert_path = "./my_custom_bert"
-    bert_tokenizer = AutoTokenizer.from_pretrained(bert_path)
-    bert_model = AutoModelForSequenceClassification.from_pretrained(bert_path)
+bert_path = "./my_custom_bert"
+bert_tokenizer = AutoTokenizer.from_pretrained(bert_path)
+bert_model = AutoModelForSequenceClassification.from_pretrained(bert_path)
         
     
     
@@ -30,10 +30,22 @@ def chatbot():
             print("今日もお疲れ様でした.")
             break
         conversation_history.append({"role": "user", "content": user_input})
-        load_label_data(label_path, label_path1)
         
+        # 入力文の自己開示判定
+        inputs = bert_tokenizer(user_input, return_tensors = "pt")
+        with torch.no_grad():
+            outputs = bert_model(**inputs)
+            prediction = torch.argmax(outputs.logits, dim=-1).item()
+            is_bert_judge = (prediction == 1)
+            
+            
+        if is_bert_judge:
+            judge_prompt = "自己開示有り"
+        else:
+            judge_prompt = "自己開示無し"
+        system_prompt = f"{base_prompt}\n : {judge_prompt}" 
         response = client.responses.create(
-            model="gpt-5-mini", instructions=instructions, input=user_input
+            model="gpt-5-mini", instructions=system_prompt, input=conversation_history
         )
         print(f"GPT:  {response.output_text}")
         conversation_history.append(
